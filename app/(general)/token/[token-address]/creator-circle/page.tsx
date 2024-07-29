2959387c61b63fb51261920bc2c5f448b2a8daa1
tnrdd <tnrdd@tutanota.com>
2024-07-29 07:58:07 +0200

    feat: style creator circle page

--- app/(general)/token/[token-address]/creator-circle/page.tsx
+++ app/(general)/token/[token-address]/creator-circle/page.tsx
@@ -6,6 +6,7 @@ import { Framework, NativeAssetSuperToken } from "@superfluid-finance/sdk-core";
 import Link from "next/link";
 import { formatEther } from "viem";
 import { Address, useAccount, useContractRead, useNetwork } from "wagmi";
+import { useMediaQuery } from "react-responsive";
 
 import { nativeStewardLicenseFacetABI } from "@/lib/blockchain";
 import { useEthersProvider, useEthersSigner } from "@/lib/hooks/use-ethers-js";
@@ -28,6 +29,8 @@ export default function CreatorCirclePage({
   const [superTokenBalance, setSuperTokenBalance] = useState<string>();
   const [sfFramework, setSfFramework] = useState<Framework>();
   const [amountsReceived, setAmountsReceived] = useState<string[]>([]);
+
+  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
 
   const CREATOR_CIRCLE_QUERY = gql`
     query CreatorCircle($publisher: String!) {
@@ -186,99 +189,146 @@ export default function CreatorCirclePage({
   };
 
   return (
-    <div className="w-10/12 lg:w-9/12">
-      <h1 className="text-4xl font-bold text-blue-500">Creator Circle</h1>
-      <h2 className="text-medium mt-5 text-2xl font-bold">{tokenName}</h2>
-      <h3 className="text-medium mt-12 text-xl font-bold">Allocation Table</h3>
-      <table className="mt-4 w-full border-separate border-spacing-4">
-        <thead>
-          <tr>
-            <th>Address</th>
-            <th>Units</th>
-            <th>%</th>
-            <th className="lg:hidden">Honorarium</th>
-            <th className="hidden lg:block">To-Date Honorarium</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {creatorCircle?.indexes[0]?.subscriptions.map(
-            (subscription: Subscription, i: number) => (
-              <tr key={i} className="">
-                <td className="rounded-lg bg-gray-300 p-2 dark:bg-gray-700">
-                  {truncateStr(subscription.subscriber.id, 20)}
-                </td>
-                <td className="rounded-lg bg-gray-300 p-2 dark:bg-gray-700">
-                  {subscription.units}
-                </td>
-                <td className="rounded-lg bg-gray-300 p-2 dark:bg-gray-700">
-                  {parseFloat(
-                    ((Number(subscription.units) * 100) / totalUnits).toFixed(2)
-                  )}
-                </td>
-                <td className="rounded-lg bg-gray-300 p-2 dark:bg-gray-700">
-                  {amountsReceived[i] ? amountsReceived[i] : "0"}
-                </td>
-                {subscription.subscriber.id ===
-                  account.address?.toLowerCase() && !subscription.approved ? (
-                  <td className="flex justify-center">
-                    <button
-                      className="w-28 rounded-lg bg-green-500 px-5 py-2 font-bold lg:w-40"
-                      onClick={handleClaim}
-                    >
-                      {isApproving ? (
-                        <span className="lds-dual-ring" />
-                      ) : (
-                        <>
-                          <span className="hidden lg:block">Approve Units</span>
-                          <span className="lg:hidden">Approve</span>
-                        </>
+    <>
+      <h1 className="font-mono text-5xl sm:text-[75px] xl:text-[100px] 2xl:text-[128px] text-center leading-none mt-12 sm:mt-16 xl:mt-20 2xl:mt-24 min-[2000px]:mt-32">
+        Creator Circle
+      </h1>
+      <div className="flex flex-col items-center max-w-[320px] sm:max-w-[750px] xl:max-w-[1100px] 2xl:max-w-[1200px] m-auto">
+        <div className="w-[320px] sm:w-[600px] xl:w-[750px] 2xl:w-[850px] my-10 sm:mt-16 xl:mt-20 2xl:mt-24">
+          <h3 className="text-xl font-bold">Allocation Table</h3>
+          {isMobile && (
+            <>
+              {creatorCircle?.indexes[0]?.subscriptions.map(
+                (subscription: Subscription, i: number) => (
+                  <div key={i} className="flex flex-col">
+                    <span className="pt-3">Address</span>
+                    <span className="pt-2 border-b border-black font-serif text-2xl">
+                      {truncateStr(subscription.subscriber.id, 20)}
+                    </span>
+                    <span className="pt-3">Address</span>
+                    <span className="pt-2 border-b border-black font-serif text-2xl">
+                      {subscription.units}
+                    </span>
+                    <span className="pt-3">Percentage</span>
+                    <span className="pt-2 border-b border-black font-serif text-2xl">
+                      {parseFloat(
+                        (
+                          (Number(subscription.units) * 100) /
+                          totalUnits
+                        ).toFixed(2)
                       )}
-                    </button>
-                  </td>
-                ) : null}
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
-      {account?.address ? (
-        <div className="mt-12">
-          <h4 className="text-xl font-bold">Your Available Honorarium</h4>
-          <p className="mt-2 leading-6">
-            The Honorariums from all Creator Circles in which you have approved
-            units are collected in the single balance shown below.
-            <br /> You can utilize this{" "}
-            <Link
-              href="https://docs.superfluid.finance/superfluid/developers/super-tokens"
-              target="_blank"
-              prefetch={false}
-              className="underline"
-            >
-              Super Token
-            </Link>{" "}
-            balance as is or withdraw to ETH.
-          </p>
-          <h5 className="mt-5 text-lg font-bold">
-            {superTokenBalance
-              ? formatEther(BigInt(superTokenBalance)).slice(0, 10)
-              : 0}{" "}
-            ETHx
-          </h5>
-          {superTokenBalance && BigInt(superTokenBalance) > 0 ? (
-            <button
-              className="mt-3 w-48 rounded-lg bg-green-500 px-5 py-2 font-bold"
-              onClick={handleWithdraw}
-            >
-              {isWithdrawing ? (
-                <span className="lds-dual-ring" />
-              ) : (
-                "Withdraw Funds"
+                    </span>
+                    <span className="pt-3">Amount</span>
+                    <span className="pt-2 border-b border-black font-serif text-2xl">
+                      {amountsReceived[i] ? amountsReceived[i] : "0"}
+                    </span>
+                    {subscription.subscriber.id ===
+                      account.address?.toLowerCase() &&
+                    !subscription.approved ? (
+                      <button
+                        className="bg-neon-green px-2 py-1 font-serif text-2xl"
+                        onClick={handleClaim}
+                      >
+                        <span>
+                          {isApproving ? "Approving..." : "Approve Units"}
+                        </span>
+                      </button>
+                    ) : null}
+                  </div>
+                )
               )}
-            </button>
+            </>
+          )}
+          {!isMobile && (
+            <table className="mt-5 w-full">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Units</th>
+                  <th>%</th>
+                  <th className="lg:hidden">Honorarium</th>
+                  <th className="hidden lg:block">To-Date Honorarium</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {creatorCircle?.indexes[0]?.subscriptions.map(
+                  (subscription: Subscription, i: number) => (
+                    <>
+                      <tr key={i} className="font-serif text-2xl">
+                        <td className="pt-3 border-b border-black text-center">
+                          {truncateStr(subscription.subscriber.id, 20)}
+                        </td>
+                        <td className="pt-3 border-b border-black text-center">
+                          {subscription.units}
+                        </td>
+                        <td className="pt-3 border-b border-black text-center">
+                          {parseFloat(
+                            (
+                              (Number(subscription.units) * 100) /
+                              totalUnits
+                            ).toFixed(2)
+                          )}
+                        </td>
+                        <td className="pt-3 border-b border-black text-center">
+                          {amountsReceived[i] ? amountsReceived[i] : "0"}
+                        </td>
+                        {subscription.subscriber.id ===
+                          account.address?.toLowerCase() &&
+                        !subscription.approved ? (
+                          <td className="flex justify-center pt-3">
+                            <button
+                              className="bg-neon-green px-2 py-1"
+                              onClick={handleClaim}
+                            >
+                              <span>
+                                {isApproving ? "Approving..." : "Approve Units"}
+                              </span>
+                            </button>
+                          </td>
+                        ) : null}
+                      </tr>
+                    </>
+                  )
+                )}
+              </tbody>
+            </table>
+          )}
+          {account?.address ? (
+            <div className="mt-12 mb-24 2xl:mb-32">
+              <h4 className="text-xl font-bold">Your Available Honorarium</h4>
+              <p className="mt-2 leading-6">
+                The Honorariums from all Creator Circles in which you have
+                approved units are collected in the single balance shown below.
+                <br /> You can utilize this{" "}
+                <Link
+                  href="https://docs.superfluid.finance/superfluid/developers/super-tokens"
+                  target="_blank"
+                  prefetch={false}
+                  className="underline"
+                >
+                  Super Token
+                </Link>{" "}
+                balance as is or withdraw to ETH.
+              </p>
+              <h5 className="mt-5 text-lg font-serif text-2xl">
+                {superTokenBalance
+                  ? formatEther(BigInt(superTokenBalance)).slice(0, 10)
+                  : 0}{" "}
+                ETHx
+              </h5>
+              {superTokenBalance && BigInt(superTokenBalance) > 0 ? (
+                <button
+                  className="mt-3 w-full bg-neon-green py-1 font-serif text-2xl"
+                  onClick={handleWithdraw}
+                >
+                  {isWithdrawing ? "Witdrawing..." : "Withdraw Funds"}
+                </button>
+              ) : null}
+            </div>
           ) : null}
         </div>
-      ) : null}
-    </div>
+      </div>
+    </>
   );
 }
