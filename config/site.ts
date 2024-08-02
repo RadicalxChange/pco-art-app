7fd1c55a82fa88b8d907bb9793f1cb367be3f8b0
tnrdd <tnrdd@tutanota.com>
2024-08-02 07:28:25 +0200

    fix: opengraph site title

--- config/site.ts
+++ config/site.ts
@@ -8,7 +8,7 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-  name: "PCO Art",
+  name: "PCO: A stewardship technology for art",
   title: "PCO: A stewardship technology for art",
   description:
     "Mint a stewardship licence for your artworks today! Art enriches society through the weaving of relations between the cultures that create it and the cultures that receive it. Partial common ownership of art is a new evolving system that allows artists, communities and holders of art to create structures of shared ownership and distribution of value that reflects those living relationships.",
